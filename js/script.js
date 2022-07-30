7b8ccadccbd33e54a907713176b4a3dd9e6fd29c
Bartosz Peciak <bartosz.peciak95@gmail.com>
2022-07-30 18:01:16 +0200

    added sign-up field and checkForm script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const signUpBtn = document.querySelector('.sign-up__bottom-btn')
+const signUpInput = document.querySelector('.sign-up__bottom-input')
+const error = document.querySelector('.error')
+console.log(error)
+const checkInput = () => {
+	if (signUpInput.value === '') {
+		error.style.visibility = 'visible'
+	} else {
+		error.style.visibility = 'hidden'
+	}
+}
+
+signUpBtn.addEventListener('click', checkInput)
