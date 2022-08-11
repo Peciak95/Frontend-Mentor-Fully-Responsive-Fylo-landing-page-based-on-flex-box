3fe138021832f6c4a1312c1f168512a0c94b0481
Bartosz Peciak <bartosz.peciak95@gmail.com>
2022-08-11 11:17:42 +0200

    deleted console log from js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const signUpBtn = document.querySelector('.sign-up__bottom-btn')
 const signUpInput = document.querySelector('.sign-up__bottom-input')
 const error = document.querySelector('.error')
-console.log(error)
 const checkInput = () => {
 	if (signUpInput.value === '') {
 		error.style.visibility = 'visible'
